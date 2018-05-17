d85ef12364a2cf3e76efe8849bce9841368b2173
Alex Wilson <fonzmp@gmail.com>
2018-05-17 23:35:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
   end
 
   def self.find_or_create_by_name(title)
-    if !find_by_name(title)
+    if find_by_name(title)
       create_by_name(title)
     else
       find_by_name(title)
