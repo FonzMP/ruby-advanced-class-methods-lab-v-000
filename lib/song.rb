9379ec4876756b5099c4389e4917c2a0a4171bfb
Alex Wilson <fonzmp@gmail.com>
2018-05-17 23:26:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,8 +42,8 @@ class Song
   def self.find_or_create_by_name(title)
     @@all.each do |names|
       if names.name == title
-        x = self.new
-        x.find_by_name(title)
+
+        self.find_by_name(title)
       else
         self.create_by_name(title)
       end
