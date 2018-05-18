a4794d03e5c61f1fcdaf96d7569336dc3801b682
Alex Wilson <fonzmp@gmail.com>
2018-05-18 00:24:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,9 +43,7 @@ class Song
     find_by_name(title)
     create_by_name(title)
     @@all.each do |item|
-      if item.name == title
-        puts item.name
-      end
+      puts item.name
     end
   end
 
