864a2794dc5957e9e41949d25804e01f162a9c16
Alex Wilson <fonzmp@gmail.com>
2018-05-18 01:00:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   def self.find_by_name(title)
     @@all.each do |item|
       puts item.name.class
-      puts title
+      puts title.class
       if title == item.name
         item.name
       end
